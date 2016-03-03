03961a2d4b73012814499cc31c239d1d406320f4
amanjain <91aman@gmail.com>
2016-03-03 11:02:53 +0530

    [new site] : add more skills @WIP

--- Site2/build/app.js
+++ Site2/build/app.js
@@ -52084,6 +52084,66 @@
 	        label: 'Less',
 	        logo: 'http://www.arrowhitech.com/wp-content/uploads/2014/10/less-framework-technology-377x200.png',
 	        url: 'http://lesscss.org/'
+	    },
+	    redux: {
+	        id: 'redux',
+	        label: 'Redux',
+	        logo: 'https://raw.githubusercontent.com/wiki/facebook/react/react-logo-1000-transparent.png',
+	        url: 'http://redux.js.org/'
+	    },
+	    flux: {
+	        id: 'flux',
+	        label: 'flux',
+	        logo: 'http://objectpartners.github.io/react-workshop/assets/img/flux-logo.png',
+	        url: 'https://facebook.github.io/flux/'
+	    },
+	    d3: {
+	        id: 'd3',
+	        label: 'D3',
+	        logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/1/15/Logo_D3.svg/100px-Logo_D3.svg.png',
+	        url: 'https://d3js.org/'
+	    },
+	    highCharts: {
+	        id: 'highCharts',
+	        label: 'High Charts',
+	        logo: 'http://forum.highcharts.com/apple-touch-icon-180x180.png',
+	        url: 'http://www.highcharts.com/'
+	    },
+	    gulp: {
+	        id: 'gulp',
+	        label: 'Gulp',
+	        logo: 'http://www.codeproject.com/KB/HTML/865943/gulp.png',
+	        url: 'http://gulpjs.com/'
+	    },
+	    git: {
+	        id: 'git',
+	        label: 'Git',
+	        logo: 'https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png',
+	        url: 'https://git-scm.com/'
+	    },
+	    sublime: {
+	        id: 'sublime',
+	        label: 'Sublime',
+	        logo: 'https://upload.wikimedia.org/wikipedia/en/4/4c/Sublime_Text_Logo.png',
+	        url: 'https://www.sublimetext.com/'
+	    },
+	    intellij: {
+	        id: 'intellij',
+	        label: 'Intellij',
+	        logo: 'http://osx.vn/attachments/ij-png.4895/',
+	        url: 'https://www.jetbrains.com/idea/'
+	    },
+	    npm: {
+	        id: 'npm',
+	        label: 'npm',
+	        logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Npm-logo.svg/320px-Npm-logo.svg.png',
+	        url: 'https://www.npmjs.com/'
+	    },
+	    node: {
+	        id: 'node',
+	        label: 'nodejs',
+	        logo: 'http://tomnora.com/wp-content/uploads/2016/01/nodejs-logo.png',
+	        url: 'https://nodejs.org/en/'
 	    }
 
 	};
